b59cb328c1a169ac055f5a6bbc211d9e8d6ade50
fukumori.masato <fukumori.masato.dev@gmail.com>
2019-01-05 12:09:40 +0900

    emacs: change el-get install method (el-get-install.el -> package-install from melpa)

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -76,11 +76,10 @@
 ;;;
 (add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
 (unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
+  (package-install 'el-get)
+  (require 'el-get))
+
+(el-get 'sync)
 
 ;;;
 ;;;  color theme
